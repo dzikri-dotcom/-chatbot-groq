715b32161eb72caff32082e13e020884bfd2249c
Dzikri Lathiful Qodim <dzikrilathifulqodim@gmail.com>
2026-05-04 16:54:17 +0700

    Change bot name from ChefBot to TesBot

--- src/config/chatbotConfig.ts
+++ src/config/chatbotConfig.ts
@@ -1,7 +1,7 @@
 import type { ChatConfig } from "../types/Message";
  
 const chatbotConfig: ChatConfig = {
-  botName: "ChefBot",
+  botName: "TesBot",
   welcomeMessage:
     "Halo! Saya ChefBot, asisten rekomendasi menu restoran Anda. " +
     "Ceritakan preferensi atau mood makan Anda, dan saya akan " +
